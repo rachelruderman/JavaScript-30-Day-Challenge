cf917d8e22cc464e4728449bba24fb76776e38a4
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-24 13:12:56 -0700

    11 HTML Vid Player, created togglePlay() with string variable

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,5 +8,17 @@
   const skipButtons = player.querySelectorAll('[data-skip]')
   const ranges = player.querySelectorAll('.player__slider')
 //Build Our Functions//
+function togglePlay(){
+  const method = video.paused ? 'play' : 'pause'
+  //because the method name is in a string here, we can access the method name and call it
+  video[method]()
+  //call it
+  // if(video.paused){
+    //paused is a property that lives on the video; there is no playing property
+  //   video.play()
+  // }else{
+  //   video.pause()
+}
+}
 
 //Hook Up Event Listeners//
