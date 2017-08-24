4589766b1df5c18f52e36c6cb7b8c10ecd8385fd
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-24 13:17:09 -0700

    11 HTML Vid Player, added event listeners to toggle vid play

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,10 +3,10 @@
   const video = player.querySelector('.viewer')
   const progress = player.querySelector('.progress')
   const progressBar = player.querySelector('.progress__filled')
-
   const toggle = player.querySelector('.toggle')
   const skipButtons = player.querySelectorAll('[data-skip]')
   const ranges = player.querySelectorAll('.player__slider')
+
 //Build Our Functions//
 function togglePlay(){
   const method = video.paused ? 'play' : 'pause'
@@ -18,7 +18,9 @@ function togglePlay(){
   //   video.play()
   // }else{
   //   video.pause()
-}
+// }
 }
 
 //Hook Up Event Listeners//
+video.addEventListener('click', togglePlay)
+toggle.addEventListener('click', togglePlay)
