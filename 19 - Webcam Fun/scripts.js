e6a781edc471e81595c490a8964a2d4a512ba05d
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-29 12:35:42 -0700

    19 webcam, access local webcam

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,14 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo(){
+  //this is how you get acces to a user's video
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  //this returns a promise
+    .then(localMediaStream => {
+      console.log(localMediaStream)
+    })
+}
+
+getVideo()
