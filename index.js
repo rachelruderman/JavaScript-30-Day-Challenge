599b524eb342c7393810a73d613c69484d4f0d6a
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-03 13:08:37 -0700

    testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
 // app.use("/styles", express.static(__dirname));
-app.use("/sounds", express.static(__dirname + '/sounds'));
+// app.use("/sounds", express.static(__dirname + '/sounds'));
 // app.use("/scripts", express.static(__dirname + '/scripts'));
 
 // viewed at based directory http://localhost:8080/
@@ -12,5 +12,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + 'index.html'));
 });
 
+// add other routes below
+app.get('/drumkit', function (req, res) {
+  res.sendFile(path.join(__dirname + '/01 - JavaScript Drum Kit/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
